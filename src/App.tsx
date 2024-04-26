596bb90cc83abfb4499f3af27d1283b1392523aa
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-04-26 22:12:53 +0530

    Initial setup of App component with clock, break, and session sections

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [count, setCount] = useState(0)
 
-  return (
-    <>
-     
-    </>
-  )
+ return (
+   <>
+     <div className="clock">
+       <div className="setters">
+         <div className="break">
+           <h4 id="break-label">Break Length</h4>
+         </div>
+         <div className="session">
+           <h4 id="session-label">Session Length</h4>
+         </div>
+       </div>
+     </div>
+   </>
+ )
 }
 
 export default App
